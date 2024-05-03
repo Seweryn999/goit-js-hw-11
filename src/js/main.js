296232551d96daa3136a06ffe8d953b827326730
Seweryn <sewcio0212@gmail.com>
2024-05-03 15:49:16 +0200

    import simplelightbox dedeclared in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
+// import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { searchPhrase } from './requests';
 
